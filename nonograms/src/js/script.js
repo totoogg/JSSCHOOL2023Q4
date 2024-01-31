751c1ab97e0abcd7078445902c5ef42479aac96d
totoogg <goncharov.vova2014@gamil.com>
2024-01-31 20:18:30 +0300

    feat: add functions change sound, theams and style for media

--- nonograms/src/js/script.js
+++ nonograms/src/js/script.js
@@ -2,6 +2,7 @@ import schemes from './modules/schemes.json' assert { type: 'json' };
 
 let time;
 let isTime = false;
+let isPlay = true;
 
 window.onload = function () {
   buildStartPage();
@@ -63,11 +64,19 @@ const checkWin = () => {
     let finishSquare = game.querySelectorAll('.row__data[data-true]').length;
 
     if (brillSquare === finishSquare && brillSquareTrue === finishSquare) {
+      if (isPlay) {
+        musicPlay('src/sounds/win.mp3');
+      }
       clearInterval(time);
       isTime = false;
       showModal();
     }
   }
+};
+
+const musicPlay = (src) => {
+  const audio = new Audio(src);
+  audio.play();
 };
 
 const clickSquare = () => {
@@ -81,10 +90,16 @@ const clickSquare = () => {
       if (!td || !td.getAttribute('data-click')) return;
 
       if (event.button === 2) {
+        if (isPlay) {
+          musicPlay('src/sounds/clickRight.mp3');
+        }
         td.classList.remove('brill');
         td.classList.toggle('active');
         return;
       } else {
+        if (isPlay) {
+          musicPlay('src/sounds/clickLeft.mp3');
+        }
         td.classList.remove('active');
         td.classList.toggle('brill');
       }
@@ -440,6 +455,60 @@ const createTimer = () => {
   block.append(p);
 };
 
+const createButtonThemes = () => {
+  let block = document.querySelector('.media');
+
+  const div = document.createElement('div');
+  div.classList.add('media__themes');
+  div.classList.add('sun');
+  div.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 63 63" style="enable-background:new 0 0 63 63;" xml:space="preserve">
+  <g>
+    <g id="group-6svg">
+      <path id="path-1_26_" d="M32.524,63c-8.687,0-16.855-3.383-22.998-9.526S0,39.164,0,30.476c0-8.687,3.382-16.855,9.526-22.998    c3.248-3.248,7.095-5.732,11.432-7.38c0.554-0.209,1.176-0.076,1.593,0.341c0.418,0.418,0.552,1.042,0.342,1.594    c-4.128,10.864-1.499,23.18,6.697,31.376c5.565,5.564,12.967,8.629,20.842,8.629c3.619,0,7.163-0.65,10.534-1.931    c0.556-0.209,1.177-0.076,1.594,0.342c0.418,0.417,0.552,1.041,0.342,1.593c-1.648,4.338-4.132,8.184-7.381,11.432    C49.379,59.617,41.211,63,32.524,63z M19.044,4.222c-2.738,1.396-5.216,3.197-7.397,5.378C6.071,15.176,3,22.59,3,30.476    c0,7.887,3.071,15.3,8.647,20.877C17.223,56.929,24.637,60,32.524,60c7.886,0,15.3-3.071,20.876-8.647    c2.181-2.18,3.981-4.658,5.377-7.395c-2.715,0.718-5.509,1.08-8.345,1.08c-8.677,0-16.832-3.377-22.963-9.507    C19.249,27.311,16.111,15.331,19.044,4.222z"/>
+    </g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>`;
+  block.append(div);
+  div.addEventListener('click', () => {
+    if (div.classList.contains('sun')) {
+      div.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 612 612" style="fill: white;enable-background:new 0 0 612 612;" xml:space="preserve">
+      <g>
+        <g id="_x37__5_">
+          <g>
+            <path d="M76.5,286.875H19.125C8.568,286.875,0,295.443,0,306c0,10.557,8.568,19.125,19.125,19.125H76.5     c10.557,0,19.125-8.568,19.125-19.125C95.625,295.443,87.057,286.875,76.5,286.875z M306,95.625     c10.557,0,19.125-8.568,19.125-19.125V19.125C325.125,8.568,316.557,0,306,0c-10.557,0-19.125,8.568-19.125,19.125V76.5     C286.875,87.057,295.443,95.625,306,95.625z M490.002,148.792l40.182-40.182c7.401-7.401,7.401-19.393,0-26.794     s-19.394-7.401-26.795,0l-40.182,40.182c-7.401,7.401-7.401,19.393,0,26.794C470.609,156.194,482.601,156.194,490.002,148.792z      M141.716,443.509l-40.182,40.182c-7.401,7.401-7.401,19.393,0,26.794s19.393,7.401,26.794,0l40.182-40.182     c7.401-7.401,7.401-19.393,0-26.794S149.118,436.107,141.716,443.509z M130.203,157.246c7.478,7.478,19.584,7.478,27.042,0     c7.459-7.478,7.459-19.584,0-27.042L116.682,89.62c-7.478-7.478-19.584-7.478-27.043,0c-7.478,7.478-7.478,19.584,0,27.043     L130.203,157.246z M306,516.375c-10.557,0-19.125,8.568-19.125,19.125v57.375c0,10.557,8.568,19.125,19.125,19.125     c10.557,0,19.125-8.568,19.125-19.125V535.5C325.125,524.943,316.557,516.375,306,516.375z M481.797,454.754     c-7.478-7.478-19.584-7.478-27.043,0c-7.478,7.479-7.478,19.584,0,27.043l40.564,40.564c7.478,7.478,19.584,7.478,27.043,0     c7.459-7.479,7.478-19.584,0-27.043L481.797,454.754z M592.875,286.875H535.5c-10.557,0-19.125,8.568-19.125,19.125     c0,10.557,8.568,19.125,19.125,19.125h57.375c10.557,0,19.125-8.568,19.125-19.125C612,295.443,603.432,286.875,592.875,286.875z      M306,133.76c-95.128,0-172.24,77.112-172.24,172.24S210.872,478.24,306,478.24S478.24,401.128,478.24,306     S401.128,133.76,306,133.76z M306,439.837c-73.918,0-133.837-59.919-133.837-133.837S232.082,172.163,306,172.163     S439.837,232.082,439.837,306S379.918,439.837,306,439.837z"/>
+          </g>
+        </g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>`;
+      div.classList.remove('sun');
+      div.classList.add('night');
+    } else {
+      div.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" id="Capa_1" x="0px" y="0px" viewBox="0 0 63 63" style="enable-background:new 0 0 63 63;" xml:space="preserve">
+  <g>
+    <g id="group-6svg">
+      <path id="path-1_26_" d="M32.524,63c-8.687,0-16.855-3.383-22.998-9.526S0,39.164,0,30.476c0-8.687,3.382-16.855,9.526-22.998    c3.248-3.248,7.095-5.732,11.432-7.38c0.554-0.209,1.176-0.076,1.593,0.341c0.418,0.418,0.552,1.042,0.342,1.594    c-4.128,10.864-1.499,23.18,6.697,31.376c5.565,5.564,12.967,8.629,20.842,8.629c3.619,0,7.163-0.65,10.534-1.931    c0.556-0.209,1.177-0.076,1.594,0.342c0.418,0.417,0.552,1.041,0.342,1.593c-1.648,4.338-4.132,8.184-7.381,11.432    C49.379,59.617,41.211,63,32.524,63z M19.044,4.222c-2.738,1.396-5.216,3.197-7.397,5.378C6.071,15.176,3,22.59,3,30.476    c0,7.887,3.071,15.3,8.647,20.877C17.223,56.929,24.637,60,32.524,60c7.886,0,15.3-3.071,20.876-8.647    c2.181-2.18,3.981-4.658,5.377-7.395c-2.715,0.718-5.509,1.08-8.345,1.08c-8.677,0-16.832-3.377-22.963-9.507    C19.249,27.311,16.111,15.331,19.044,4.222z"/>
+    </g>
+  </g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g><g></g></svg>`;
+      div.classList.add('sun');
+      div.classList.remove('night');
+    }
+  });
+};
+
+const createButtonSound = () => {
+  let block = document.querySelector('.media');
+
+  const div = document.createElement('div');
+  div.classList.add('media__sound');
+  div.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><defs><style></style></defs><title/><g data-name="Layer 34" id="Layer_34"><path class="cls-1" d="M18,29a1,1,0,0,1-.57-.18l-10-7A1,1,0,0,1,7,21V11a1,1,0,0,1,.43-.82l10-7a1,1,0,0,1,1-.07A1,1,0,0,1,19,4V28a1,1,0,0,1-.54.89A1,1,0,0,1,18,29ZM9,20.48l8,5.6V5.92l-8,5.6Z"/><path class="cls-1" d="M8,22H4a3,3,0,0,1-3-3V13a3,3,0,0,1,3-3H8a1,1,0,0,1,1,1V21A1,1,0,0,1,8,22ZM4,12a1,1,0,0,0-1,1v6a1,1,0,0,0,1,1H7V12Z"/><path class="cls-1" d="M18,21V19a3,3,0,0,0,2.12-5.12l1.42-1.42A5,5,0,0,1,18,21Z"/><path class="cls-1" d="M26.48,25.48a1,1,0,0,1-.71-.29,1,1,0,0,1,0-1.42,11,11,0,0,0,0-15.54,1,1,0,1,1,1.42-1.42,13,13,0,0,1,0,18.38A1,1,0,0,1,26.48,25.48Z"/><path class="cls-1" d="M23.65,22.65a1,1,0,0,1-.7-.29A1,1,0,0,1,23,21a7,7,0,0,0,0-9.9,1,1,0,0,1,1.41-1.41,9,9,0,0,1,0,12.72A1,1,0,0,1,23.65,22.65Z"/></g></svg>`;
+  block.append(div);
+  div.addEventListener('click', () => {
+    if (isPlay) {
+      isPlay = false;
+      div.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><defs><style></style></defs><title/><g data-name="Layer 35" id="Layer_35"><path class="cls-1" d="M18,29a1,1,0,0,1-.57-.18l-10-7A1,1,0,0,1,7,21V11a1,1,0,0,1,.43-.82l10-7a1,1,0,0,1,1-.07A1,1,0,0,1,19,4V28a1,1,0,0,1-.54.89A1,1,0,0,1,18,29ZM9,20.48l8,5.6V5.92l-8,5.6Z"/><path class="cls-1" d="M8,22H4a3,3,0,0,1-3-3V13a3,3,0,0,1,3-3H8a1,1,0,0,1,1,1V21A1,1,0,0,1,8,22ZM4,12a1,1,0,0,0-1,1v6a1,1,0,0,0,1,1H7V12Z"/><rect class="cls-1" height="12" transform="translate(-3.77 22.9) rotate(-45)" width="2" x="24.76" y="10"/><rect class="cls-1" height="2" transform="translate(-3.77 22.9) rotate(-45)" width="12" x="19.76" y="15"/></g></svg>`;
+    } else {
+      isPlay = true;
+      div.innerHTML = `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 32 32"><defs><style></style></defs><title/><g data-name="Layer 34" id="Layer_34"><path class="cls-1" d="M18,29a1,1,0,0,1-.57-.18l-10-7A1,1,0,0,1,7,21V11a1,1,0,0,1,.43-.82l10-7a1,1,0,0,1,1-.07A1,1,0,0,1,19,4V28a1,1,0,0,1-.54.89A1,1,0,0,1,18,29ZM9,20.48l8,5.6V5.92l-8,5.6Z"/><path class="cls-1" d="M8,22H4a3,3,0,0,1-3-3V13a3,3,0,0,1,3-3H8a1,1,0,0,1,1,1V21A1,1,0,0,1,8,22ZM4,12a1,1,0,0,0-1,1v6a1,1,0,0,0,1,1H7V12Z"/><path class="cls-1" d="M18,21V19a3,3,0,0,0,2.12-5.12l1.42-1.42A5,5,0,0,1,18,21Z"/><path class="cls-1" d="M26.48,25.48a1,1,0,0,1-.71-.29,1,1,0,0,1,0-1.42,11,11,0,0,0,0-15.54,1,1,0,1,1,1.42-1.42,13,13,0,0,1,0,18.38A1,1,0,0,1,26.48,25.48Z"/><path class="cls-1" d="M23.65,22.65a1,1,0,0,1-.7-.29A1,1,0,0,1,23,21a7,7,0,0,0,0-9.9,1,1,0,0,1,1.41-1.41,9,9,0,0,1,0,12.72A1,1,0,0,1,23.65,22.65Z"/></g></svg>`;
+    }
+  });
+};
+
 const buildStartPage = () => {
   const body = document.querySelector('body');
 
@@ -494,4 +563,6 @@ const buildStartPage = () => {
   createButtonsSave();
   createButtonsRandomGame();
   createTimer();
+  createButtonSound();
+  createButtonThemes();
 };
