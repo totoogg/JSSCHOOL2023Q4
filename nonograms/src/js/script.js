588adc8c5b5c13c209bea19b333d5705d0003210
totoogg <goncharov.vova2014@gamil.com>
2024-02-03 16:19:52 +0300

    feat: add function create modal record and add style for modal record

--- nonograms/src/js/script.js
+++ nonograms/src/js/script.js
@@ -54,6 +54,50 @@ const createModal = () => {
   div.append(title);
 };
 
+const returnRecord = (obj) => {
+  let time = document.querySelector('.media__timer');
+  let [hours, minutes, seconds] = time.textContent.split(':').map((x) => +x);
+  let sum = seconds + minutes * 60 + hours * 60 * 60;
+  let index = +localStorage.getItem('totooggNonogramsArrIndex');
+  let name = schemes[index].name;
+  let record = {
+    name: name,
+    time: sum,
+  };
+  if (index < 5) {
+    obj.easy.push(record);
+  } else if (index < 10) {
+    obj.middle.push(record);
+  } else {
+    obj.hard.push(record);
+  }
+  for (let i in obj) {
+    obj[i].sort((a, b) => a.time - b.time);
+    if (obj[i].length > 5) {
+      obj[i].length = 5;
+    }
+  }
+  return obj;
+};
+
+const writeRecord = () => {
+  let recordList = localStorage.getItem('totooggNonogramsArrRecordList');
+  let recordObj;
+  if (recordList) {
+    recordObj = JSON.parse(
+      localStorage.getItem('totooggNonogramsArrRecordList'),
+    );
+  } else {
+    recordObj = {
+      easy: [],
+      middle: [],
+      hard: [],
+    };
+  }
+  let record = returnRecord(recordObj);
+  localStorage.setItem('totooggNonogramsArrRecordList', JSON.stringify(record));
+};
+
 const checkWin = () => {
   const game = document.querySelector('.content__game');
 
@@ -70,6 +114,7 @@ const checkWin = () => {
       clearInterval(time);
       isTime = false;
       showModal();
+      writeRecord();
     }
   }
 };
@@ -272,7 +317,7 @@ const createModalStart = () => {
           let arrName = schemes.map((x) => x.name);
           let index = arrName.indexOf(arr[i]);
           buildGame(index);
-          localStorage.setItem('nonogramsArrIndex', index);
+          localStorage.setItem('totooggNonogramsArrIndex', index);
 
           let modalList = document.querySelector('.modal-choose-list');
           modalList.classList.add('display-none');
@@ -363,25 +408,30 @@ const createButtonsSave = () => {
     });
 
     const objSave = {
-      index: localStorage.getItem('nonogramsArrIndex'),
+      index: localStorage.getItem('totooggNonogramsArrIndex'),
       arrClick: saveArr,
       time: document.querySelector('.media__timer').textContent,
     };
-    localStorage.setItem('nonogramsArrSaveGame', JSON.stringify(objSave));
+    localStorage.setItem(
+      'totooggNonogramsArrSaveGame',
+      JSON.stringify(objSave),
+    );
   });
 
   let loading = document.createElement('button');
   loading.classList.add('save-content__button-loading');
   loading.classList.add('button');
-  if (!localStorage.getItem('nonogramsArrSaveGame')) {
+  if (!localStorage.getItem('totooggNonogramsArrSaveGame')) {
     loading.classList.add('disabled');
   }
   loading.textContent = 'Continue the last game';
   div.append(loading);
 
   loading.addEventListener('click', () => {
-    if (localStorage.getItem('nonogramsArrSaveGame')) {
-      let save = JSON.parse(localStorage.getItem('nonogramsArrSaveGame'));
+    if (localStorage.getItem('totooggNonogramsArrSaveGame')) {
+      let save = JSON.parse(
+        localStorage.getItem('totooggNonogramsArrSaveGame'),
+      );
       document.querySelector('.media__timer').textContent = save.time;
       buildGame(+save.index);
       let clicks = document.querySelectorAll('.row__data[data-click]');
@@ -431,17 +481,17 @@ const createButtonsRandomGame = () => {
     isTime = false;
     document.querySelector('.media__timer').textContent = '00:00:00';
     let random = rangeRandom(0, schemes.length - 1);
-    if (random === localStorage.getItem('nonogramsArrIndex')) {
+    if (random === localStorage.getItem('totooggNonogramsArrIndex')) {
       if (random === schemes.length - 1) {
         buildGame(0);
-        localStorage.setItem('nonogramsArrIndex', 0);
+        localStorage.setItem('totooggNonogramsArrIndex', 0);
       } else {
         buildGame(random + 1);
-        localStorage.setItem('nonogramsArrIndex', random + 1);
+        localStorage.setItem('totooggNonogramsArrIndex', random + 1);
       }
     } else {
       buildGame(random);
-      localStorage.setItem('nonogramsArrIndex', random);
+      localStorage.setItem('totooggNonogramsArrIndex', random);
     }
   });
 };
@@ -509,6 +559,108 @@ const createButtonSound = () => {
   });
 };
 
+const createModalRecord = () => {
+  const body = document.querySelector('body');
+
+  let div = document.createElement('div');
+  div.classList.add('modal-record');
+  div.classList.add('display-none');
+  body.append(div);
+
+  let h2 = document.createElement('h2');
+  h2.classList.add('modal-record__headline');
+  h2.textContent = 'Record Table';
+  div.append(h2);
+
+  let buttons = document.createElement('div');
+  buttons.classList.add('modal-record__buttons');
+  div.append(buttons);
+
+  for (let i = 0; i < 3; i++) {
+    let button = document.createElement('button');
+    button.classList.add('buttons__record-button');
+    button.classList.add('button');
+    if (i === 0) {
+      button.textContent = `Easy`;
+      button.classList.add('active');
+    }
+    if (i === 1) {
+      button.textContent = `Middle`;
+    }
+    if (i === 2) {
+      button.textContent = `Hard`;
+    }
+
+    buttons.append(button);
+
+    button.addEventListener('click', () => {
+      let record = localStorage.getItem('totooggNonogramsArrRecordList');
+      if (record) {
+        let items = document.querySelectorAll('.list__item');
+        let arrRecord = JSON.parse(record);
+        if (button.textContent === 'Easy') {
+          arrRecord = arrRecord.easy;
+        } else if (button.textContent === 'Middle') {
+          arrRecord = arrRecord.middle;
+        } else {
+          arrRecord = arrRecord.hard;
+        }
+        items.forEach((x, i) => {
+          if (arrRecord[i]) {
+            x.textContent = `${arrRecord[i].name} --- ${arrRecord[i].time} seconds`;
+          } else {
+            x.textContent = '-';
+          }
+        });
+      }
+
+      let buttonsRecord = document.querySelectorAll('.buttons__record-button');
+      buttonsRecord.forEach((x) => x.classList.remove('active'));
+
+      button.classList.add('active');
+    });
+  }
+
+  let records = document.createElement('ol');
+  records.classList.add('modal-record__list');
+  div.append(records);
+
+  for (let i = 0; i < 5; i++) {
+    let record = document.createElement('li');
+    record.classList.add('list__item');
+    record.textContent = '-';
+    records.append(record);
+
+    let recordLocal = localStorage.getItem('totooggNonogramsArrRecordList');
+    if (recordLocal) {
+      let arrRecord = JSON.parse(recordLocal);
+      arrRecord = arrRecord.easy;
+      if (arrRecord[i]) {
+        record.textContent = `${arrRecord[i].name} --- ${arrRecord[i].time} seconds`;
+      }
+    }
+  }
+};
+
+const createButtonRecord = () => {
+  let block = document.querySelector('.media');
+
+  const button = document.createElement('button');
+  button.classList.add('media__record');
+  button.classList.add('button');
+  button.textContent = 'Record Table';
+  block.append(button);
+
+  createModalRecord();
+
+  button.addEventListener('click', () => {
+    let modal = document.querySelector('.modal-record');
+    modal.classList.remove('display-none');
+    let backdrop = document.querySelector('.backdrop');
+    backdrop.classList.remove('display-none');
+  });
+};
+
 const buildStartPage = () => {
   const body = document.querySelector('body');
 
@@ -527,11 +679,12 @@ const buildStartPage = () => {
 
   backdrop.addEventListener('click', () => {
     if (
-      localStorage.getItem('nonogramsArrIndex') &&
+      localStorage.getItem('totooggNonogramsArrIndex') &&
       document.querySelector('.modal-finish').classList.contains('display-none')
     ) {
       backdrop.classList.add('display-none');
       document.querySelector('.modal-start').classList.add('display-none');
+      document.querySelector('.modal-record').classList.add('display-none');
       document
         .querySelector('.modal-choose-list')
         .classList.add('display-none');
@@ -553,7 +706,7 @@ const buildStartPage = () => {
   button.classList.add('content__button');
   content.append(button);
 
-  localStorage.removeItem('nonogramsArrIndex');
+  localStorage.removeItem('totooggNonogramsArrIndex');
 
   buildGame(0);
 
@@ -565,4 +718,5 @@ const buildStartPage = () => {
   createTimer();
   createButtonSound();
   createButtonThemes();
+  createButtonRecord();
 };
