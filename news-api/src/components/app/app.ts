1975f2b327d4433695886b8b61051f8be7aee6c9
totoogg <goncharov.vova2014@gamil.com>
2024-02-24 16:39:50 +0300

    feat: add type added types and interfaces to the App class

--- news-api/src/components/app/app.ts
+++ news-api/src/components/app/app.ts
@@ -1,7 +1,55 @@
 import AppController from '../controller/controller';
 import { AppView } from '../view/appView';
+interface IDataSources {
+    name: string;
+    id: string;
+}
+interface ISources {
+    draw(data: IDataSources[]): void;
+}
+interface ISourcesNews {
+    id: string;
+    name: string;
+}
 
-class App {
+interface IDataNews {
+    author: string;
+    content: string;
+    description: string;
+    publishedAt: string;
+    title: string;
+    url: string;
+    urlToImage: string;
+    source: ISourcesNews;
+}
+interface INews {
+    draw(data: IDataNews[]): void;
+}
+interface IDrawNewsData {
+    articles: IDataNews[];
+}
+interface IDrawSourcesData {
+    sources: IDataSources[];
+}
+interface IAppView {
+    news: INews;
+    sources: ISources;
+    drawNews(data: IDrawNewsData): void;
+    drawSources(data: IDrawSourcesData): void;
+}
+interface IController {
+    getSources(callback: (data?: IDrawSourcesData) => void): void;
+    getNews(e: Event, callback: (data?: IDrawNewsData) => void): void;
+}
+interface IApp {
+    controller: IController;
+    view: IAppView;
+    start(): void;
+}
+
+class App implements IApp {
+    public controller: IController;
+    public view: IAppView;
     constructor() {
         this.controller = new AppController();
         this.view = new AppView();
--- news-api/src/index.ts
+++ news-api/src/index.ts
@@ -1,5 +1,52 @@
 import App from './components/app/app';
 import './global.css';
 
-const app = new App();
+interface IDataSources {
+    name: string;
+    id: string;
+}
+interface ISources {
+    draw(data: IDataSources[]): void;
+}
+interface ISourcesNews {
+    id: string;
+    name: string;
+}
+
+interface IDataNews {
+    author: string;
+    content: string;
+    description: string;
+    publishedAt: string;
+    title: string;
+    url: string;
+    urlToImage: string;
+    source: ISourcesNews;
+}
+interface INews {
+    draw(data: IDataNews[]): void;
+}
+interface IDrawNewsData {
+    articles: IDataNews[];
+}
+interface IDrawSourcesData {
+    sources: IDataSources[];
+}
+interface IAppView {
+    news: INews;
+    sources: ISources;
+    drawNews(data: IDrawNewsData): void;
+    drawSources(data: IDrawSourcesData): void;
+}
+interface IController {
+    getSources(callback: (data?: IDrawSourcesData) => void): void;
+    getNews(e: Event, callback: (data?: IDrawNewsData) => void): void;
+}
+interface IApp {
+    controller: IController;
+    view: IAppView;
+    start(): void;
+}
+
+const app: IApp = new App();
 app.start();
