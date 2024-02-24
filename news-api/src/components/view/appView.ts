d8433b7356eaf5ed4a171803a76e47dccb680c4a
totoogg <goncharov.vova2014@gamil.com>
2024-02-24 16:16:11 +0300

    feat: add type added types and interfaces to the AppNews class

--- news-api/src/components/view/appView.ts
+++ news-api/src/components/view/appView.ts
@@ -1,19 +1,59 @@
 import News from './news/news';
 import Sources from './sources/sources';
 
-export class AppView {
+interface IDataSources {
+    name: string;
+    id: string;
+}
+interface ISources {
+    draw(data: IDataSources[]): void;
+}
+interface ISourcesNews {
+    id: string;
+    name: string;
+}
+
+interface IDataNews {
+    author: string;
+    content: string;
+    description: string;
+    publishedAt: string;
+    title: string;
+    url: string;
+    urlToImage: string;
+    source: ISourcesNews;
+}
+interface INews {
+    draw(data: IDataNews[]): void;
+}
+interface IDrawNewsData {
+    articles: IDataNews[];
+}
+interface IDrawSourcesData {
+    sources: IDataSources[];
+}
+interface IAppView {
+    news: INews;
+    sources: ISources;
+    drawNews(data: IDrawNewsData): void;
+    drawSources(data: IDrawSourcesData): void;
+}
+
+export class AppView implements IAppView {
+    public news: INews;
+    public sources: ISources;
     constructor() {
         this.news = new News();
         this.sources = new Sources();
     }
 
-    drawNews(data) {
-        const values = data?.articles ? data?.articles : [];
+    drawNews(data: IDrawNewsData) {
+        const values: IDataNews[] = data?.articles ? data?.articles : [];
         this.news.draw(values);
     }
 
-    drawSources(data) {
-        const values = data?.sources ? data?.sources : [];
+    drawSources(data: IDrawSourcesData) {
+        const values: IDataSources[] = data?.sources ? data?.sources : [];
         this.sources.draw(values);
     }
 }
