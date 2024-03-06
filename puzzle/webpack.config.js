0ebb59c4ba4ce265555b5fe3397c9c6d634b17f0
totoogg <136253396+totoogg@users.noreply.github.com>
2024-03-06 12:00:53 +0300

    feat: RSS-PZ-00 Initialization Code Formatting And Linting (#26)
    * init: start task
    
    * feat: add new ruls eslint
    
    * feat: prettier
    
    * feat: add webpack
    
    ---------
    
    Co-authored-by: totoogg <goncharov.vova2014@gamil.com>

---
+++ puzzle/webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const { merge } = require('webpack-merge');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const DotenvWebpackPlugin = require('dotenv-webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const baseConfig = {
+  entry: './src/index.ts',
+  module: {
+    rules: [
+      {
+        test: /\.(scss|css)$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          'postcss-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, './dist'),
+  },
+  stats: {
+    errorDetails: false,
+  },
+  plugins: [
+    new DotenvWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      filename: 'index.html',
+    }),
+    new CleanWebpackPlugin(),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+};
+
+module.exports = ({ mode }) => {
+  const isProductionMode = mode === 'prod';
+  const envConfig = isProductionMode
+    ? require('./webpack.prod.config')
+    : require('./webpack.dev.config');
+
+  return merge(baseConfig, envConfig);
+};
