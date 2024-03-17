06cf73129749d4e3b2b1dde7306feec12cc78063
totoogg <136253396+totoogg@users.noreply.github.com>
2024-03-17 18:00:55 +0300

    feat: add Distinct Known and Unknown Sentence Sections on Statistics Page (#57)
    Co-authored-by: totoogg <goncharov.vova2014@gamil.com>

--- puzzle/src/components/controller/listener/button/buttonResult.ts
+++ puzzle/src/components/controller/listener/button/buttonResult.ts
@@ -1,8 +1,16 @@
 import Listener from '../listener';
 import { IButtonResult } from '../../../interfaces/interfaces';
+import ElementCreation from '../../../view/util/element-creation';
 
 export default class ButtonResult extends Listener implements IButtonResult {
   public eventListener: string;
+
+  public line = new ElementCreation({
+    tag: 'div',
+    classNames: ['line'],
+    textContent: ``,
+    action: null,
+  });
 
   constructor(key: string) {
     super();
@@ -13,6 +21,8 @@ export default class ButtonResult extends Listener implements IButtonResult {
     event.preventDefault();
 
     this.hideElements();
+    this.createLine();
+    this.getResultText();
   }
 
   public hideElements(): void {
@@ -28,5 +38,44 @@ export default class ButtonResult extends Listener implements IButtonResult {
         el.classList.remove('display-none');
       }
     });
+  }
+
+  public getResultText(): void {
+    const total = Array.from(document.querySelectorAll('.field-total__line')).map(
+      (el) => el.textContent,
+    );
+    const totalResult = Array.from(document.querySelectorAll('.field-result__line')).map((el) =>
+      el.getAttribute('data-solution'),
+    );
+    const notKnow = document.querySelector('.result__not-know') as HTMLElement;
+    const know = document.querySelector('.result__know') as HTMLElement;
+
+    total.forEach((el, index) => {
+      const element = this.line.getElement()!.cloneNode(true);
+      element.lastChild!.textContent = el;
+
+      if (totalResult[index]) {
+        notKnow.append(element);
+      } else {
+        know.append(element);
+      }
+    });
+  }
+
+  public createLine(): void {
+    const sound = new ElementCreation({
+      tag: 'div',
+      classNames: ['line__sound'],
+      textContent: ``,
+      action: null,
+    });
+    const text = new ElementCreation({
+      tag: 'div',
+      classNames: ['line__text'],
+      textContent: ``,
+      action: null,
+    });
+
+    this.line.getElement()?.append(sound.getElement()!, text.getElement()!);
   }
 }
