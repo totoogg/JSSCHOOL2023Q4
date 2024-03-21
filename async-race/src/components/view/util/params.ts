706fe4aaae5354c1b19359bf1b51722765dafca7
totoogg <goncharov.vova2014@gamil.com>
2024-03-21 21:37:38 +0300

    init: start Async Race task

---
+++ async-race/src/components/view/util/params.ts
@@ -0,0 +1,6 @@
+export default const buttonContinueParams = {
+  tag: 'button',
+  classNames: ['field-buttons__check-continue', 'button', 'display-none'],
+  textContent: 'Check',
+  action: null,
+};
